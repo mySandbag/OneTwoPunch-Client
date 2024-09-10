fc69d81ad0cb2206ad8987d8a8610c0a8a7fc000
vlveqoo <vlveqoo@gmail.com>
2024-09-10 22:06:30 +0900

    Feat: canvas에 3D샌드백 모델 import

--- src/components/MainGame/MainGameCanvas.jsx
+++ src/components/MainGame/MainGameCanvas.jsx
@@ -1,10 +1,58 @@
-import { Canvas } from "@react-three/fiber";
+import { useEffect } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+
+import SandbagModel from "../3D/SandbagModel/SandbagModel";
 
 function MainGameCanvas() {
+  function CameraControls() {
+    const { camera } = useThree();
+    useEffect(() => {
+      camera.lookAt(0, 2, 0);
+    }, [camera]);
+
+    return null;
+  }
+
   return (
-    <div className="relative w-full flex-grow border-2 border-solid border-sky-200">
-      <div className="aspect-video w-full">
-        <Canvas className="h-full w-full"></Canvas>
+    <div className="relative w-full flex-grow">
+      <div className="aspect-video max-h-[90vh] w-full">
+        <Canvas
+          className="h-full w-full"
+          shadows
+          camera={{
+            position: [0, 3, 6],
+            fov: 30,
+          }}
+        >
+          <CameraControls />
+          <color attach="background" args={["teal"]} />
+
+          <ambientLight color="#FDF3C6" intensity={1.7} />
+          <directionalLight
+            castShadow
+            position={[10, 10, 10]}
+            intensity={1.3}
+            shadow-mapSize-width={1024}
+            shadow-mapSize-height={1024}
+            shadow-camera-far={100}
+            shadow-camera-near={0.1}
+            shadow-camera-left={-20}
+            shadow-camera-right={20}
+            shadow-camera-top={20}
+            shadow-camera-bottom={-20}
+            shadow-radius={70}
+          />
+
+          <SandbagModel />
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, 0, 0]}
+            receiveShadow
+          >
+            <planeGeometry args={[30, 30]} />
+            <meshPhongMaterial color="gray" shininess={10} />
+          </mesh>
+        </Canvas>
       </div>
     </div>
   );
