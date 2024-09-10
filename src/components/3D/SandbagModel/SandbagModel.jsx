ff042990dcf0db954b198629dfbc6a2009300fb3
vlveqoo <vlveqoo@gmail.com>
2024-09-10 21:23:36 +0900

    Feat: 3D샌드백모델 리액트 컴포넌트화

---
+++ src/components/3D/SandbagModel/SandbagModel.jsx
@@ -0,0 +1,30 @@
+import { useLoader } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+function SandbagModel() {
+  const sandbagGLTF = useLoader(
+    GLTFLoader,
+    "/src/assets/model/sandbag/scene.gltf",
+  );
+  const sandbagRef = useRef();
+
+  useEffect(() => {
+    sandbagGLTF.scene.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+  }, [sandbagGLTF]);
+
+  useEffect(() => {
+    if (sandbagRef.current);
+
+    sandbagRef.current.position.y = 1;
+  }, []);
+
+  return <primitive object={sandbagGLTF.scene} ref={sandbagRef} />;
+}
+
+export default SandbagModel;
