b9e0ca5312c785591dec0aff2c6ff5821d255eba
vlveqoo <vlveqoo@gmail.com>
2024-09-12 17:48:29 +0900

    Chore: 권투글러브의 움직임 관련 매직넘버들을 상수 처리

---
+++ src/constants/gloveMotionSettings.js
@@ -0,0 +1,13 @@
+export const INITIAL_GLOVE_SPEED = 0.05;
+
+export const MAX_GLOVE_REACH = 0.8;
+
+export const RIGHT_GLOVE_POSITION = {
+  INITIAL_X: 0.6,
+  INITIAL_Y: 1.6,
+  INITIAL_Z: 2.3,
+};
+export const RIGHT_GLOVE_ROTATION = {
+  INITIAL_X: 7,
+  INITIAL_Y: 20,
+};
